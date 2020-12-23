c198a0dfe16c2514a36aeb731d97bc5472272a6a
Mala-Patke <66596286+Mala-Patke@users.noreply.github.com>
2020-12-22 23:05:41 -0800

    Le funny formatting

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -14,26 +14,24 @@ module.exports = class PingCommand extends Command{
     execute(message){
         let now = Date.now();
         message.channel.send('pinging...')
-            .then(m => {
-                m.edit(``, {
-                    embed:{
-                        title:`:ping_pong: Pong!`,
-                        fields:[
-                            {
-                                name: 'Latency',
-                                value: `${Date.now()-now}ms`,
-                                inline:true
-                            },
-                            {
-                                name: 'Websocket Ping',
-                                value: `${this.client.ws.ping}ms`,
-                                inline:true
-                            }
-                        ]
-                    }
-                })
+        .then(m => {
+            m.edit(``, {
+                embed:{
+                    title:`:ping_pong: Pong!`,
+                    fields:[
+                        {
+                            name: 'Latency',
+                            value: `${Date.now()-now}ms`,
+                            inline:true
+                        },
+                        {
+                            name: 'Websocket Ping',
+                            value: `${this.client.ws.ping}ms`,
+                            inline:true
+                        }
+                    ]
+                }
             })
-        
+        })
     }
-
 }
