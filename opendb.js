81cde38714dd313e0174614e0f20b5b4d5c6fb8a
Ali Shahid <hobocirclemeditation@gmail.com>
2021-09-02 14:51:43 -0700

    Scale rethinkdb for mac

--- opendb.js
+++ opendb.js
@@ -1,5 +1,13 @@
-require('child_process').exec('rethinkdb.exe', {
-    cwd:'./database/RethinkDB',
-})
-.on('message', console.log)
-.on('error', console.error);
+const { exec } = require('child_process')
+
+if(process.platform === 'win32'){
+    exec('rethinkdb.exe', {
+        cwd:'./database/RethinkDB',
+    })
+        .on('message', console.log)
+        .on('error', console.error);
+} else if (process.platform === 'darwin'){
+    exec('rethinkdb')
+        .on('message', console.log)
+        .on('error', console.error)
+}
